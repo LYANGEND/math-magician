9c4345cbdc7b518541359cee21db29a439b1b1f7
lyamz <davidlyangenda@gmail.com>
2022-04-30 12:34:28 +0200

    Add events

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import Calculator from './components/Calculator';
-import Header from './components/Header';
+import React, { Component } from 'react';
+import Calculator from './components/calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Header />
-      </header>
-      <div>
-        <Calculator />
-      </div>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <Calculator />;
+  }
 }
 
 export default App;
